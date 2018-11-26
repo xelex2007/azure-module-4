960be3bff1ede3a8cd5fdf734d78bb3924fe8031
Aliaksei Kavalenka <Aliaksei_Kavalenka@epam.com>
2018-11-26 10:39:40 +0300

    new

---
+++ iis.ps1
@@ -0,0 +1,30 @@
+Configuration Main
+{
+ 
+Param ( [string] $nodeName )
+ 
+Import-DscResource -ModuleName PSDesiredStateConfiguration
+ 
+Node $nodeName
+  {
+    #Install the IIS Role
+    WindowsFeature IIS
+    {
+      Ensure = “Present”
+      Name = “Web-Server”
+    }
+ 
+    #Install ASP.NET 4.5
+    WindowsFeature ASP
+    {
+      Ensure = “Present”
+      Name = “Web-Asp-Net45”
+    }
+ 
+     WindowsFeature WebServerManagementConsole
+    {
+        Name = "Web-Mgmt-Console"
+        Ensure = "Present"
+    }
+  }
+}

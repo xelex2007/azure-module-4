4feb060e909e856cc456f65148201eb351a49a32
Aliaksei Kavalenka <Aliaksei_Kavalenka@epam.com>
2018-11-27 12:33:05 +0300

    new

--- iis.ps1
+++ iis.ps1
@@ -1,9 +1,9 @@
-Configuration Main
+﻿Configuration main
 {
  
-Param ( [string] $nodeName = "localhost" )
+Param ( [string] $nodeName = "$env:computername" )
  
-Import-DscResource -ModuleName PSDesiredStateConfiguration
+Import-DscResource -ModuleName PSDesiredStateConfiguration,xWebAdministration,xNetworking
  
 Node $nodeName
   {
@@ -26,5 +26,32 @@ Node $nodeName
         Name = "Web-Mgmt-Console"
         Ensure = "Present"
     }
+
+    xWebsite DefaultWebsite
+{
+Ensure = "Present"
+Name = "Default Web Site"
+State = "Started"
+PhysicalPath = "C:\inetpub\wwwroot"
+BindingInfo = MSFT_xWebBindingInformation
+{
+Protocol = "HTTP"
+Port = 8080
+}
+DependsOn = "[WindowsFeature]IIS"
+}
+
+xFirewall DockerSwarmTCP
+        {
+            Name        = 'allow_iis_8080'
+            DisplayName = 'Allow IIS 8080'
+            Action      = 'Allow'
+            Direction   = 'Inbound'
+            LocalPort   = ('8080')
+            Protocol    = 'TCP'
+            Profile     = 'Any'
+            Enabled     = 'True'
+}
+    
   }
 }
